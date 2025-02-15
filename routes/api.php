a8187a6ddc482fdee9a933939b03253341ff1dae
Pablo Manzano vazquez <pablomanzanovazquez@MacBook-Pro-de-Pablo.local>
2025-02-15 11:02:25 +0100

    (FEAT): Create UserController

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\UserController;
+
 
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('reviews', ReviewController::class);
 Route::apiResource(name: 'movies', controller: MovieController::class);
+Route::apiResource('users', UserController::class);
