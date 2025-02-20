d28d2fa9eff56eb6cf1f2a9cfe5ca7ee7d3120f3
Pablo Manzano vazquez <pablomanzanovazquez@MacBook-Pro-de-Pablo.local>
2025-02-20 17:04:00 +0100

    (FIX): Migration book_club entities

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
-use App\Http\Controllers\MovieController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\UserController;
 
 
 
 Route::apiResource('events', EventController::class);
-Route::apiResource('reviews',  ReviewController::class);
-Route::apiResource(name: 'movies', controller: MovieController::class);
+Route::apiResource('reviews', ReviewController::class);
+Route::apiResource(name: 'books', controller: BookController::class);
 Route::apiResource('users', UserController::class);
